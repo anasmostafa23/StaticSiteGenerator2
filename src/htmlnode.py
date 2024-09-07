9f776ee554126962e98712b91ad5e02ea605d443
Anas Afif <anasmostafa23@yahoo.com>
2024-09-07 14:53:42 +0500

    updated

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -2,15 +2,15 @@ class HTMLNode():
     def __init__(self,tag = None, value = None , children = None , props = None) :
         self.tag = tag 
         self.value = value
-        self.children = children
-        self.props = props 
+        self.children = children if children is not None else []
+        self.props = props if props is not None else {}
 
     def to_html(self) :
         raise NotImplementedError 
     
-    def props_to_html(self) :
-       for key, value in self.props.items():
-           print (f'{key}="{value}" ')
+    def props_to_html(self):
+        # Returns a string of HTML properties
+        return ' '.join(f'{key}="{value}"' for key, value in self.props.items())
 
     def __repr__(self): 
         return f"tag = {self.tag} , value = {self.value} , children = {self.children} , props = {self.props.items()}"
@@ -22,3 +22,25 @@ class HTMLNode():
                 self.value == other.value and
                 self.children == other.children and
                 self.props == other.props)
+    
+
+class LeafNode(HTMLNode):
+    def __init__ (self,tag = None, value = None, props = None , children = None) :
+        super().__init__(tag = tag, value = value, children=  children , props = props)
+       
+    def to_html(self):
+        if self.children != [] : 
+            raise Exception("Leaf node can't have children")
+        if self.value == None :
+            raise ValueError
+
+        html_opening_tag = f"<{self.tag}"
+        if self.props:
+            html_opening_tag += f" {self.props_to_html()}"
+        html_opening_tag += ">"
+
+        html_closing_tag = f"</{self.tag}>"
+
+        # Construct and return the full HTML string
+        print( f"{html_opening_tag}{self.value}{html_closing_tag}")
+        
--- src/main.py
+++ src/main.py
@@ -1,7 +1,15 @@
 from textnode import *
+from htmlnode import *
 def main () :
-    node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    repr(node)
+    leaf1 = LeafNode("p", "This is a paragraph of text.")
+    leaf2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+
+    leaf1.to_html()
+    leaf2.to_html()
+
+
+    
+    
 
 if __name__ == "__main__":
     main()
